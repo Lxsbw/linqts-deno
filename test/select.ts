c3b92723590a84cf2ba53420d8c0b6c746f51605
zhixiong <lxsbw@outlook.com>
2023-02-16 10:22:25 +0800

    test branch

---
+++ test/select.ts
@@ -0,0 +1,18 @@
+import Linq from '../src/linq.ts';
+
+const parameters = [
+  { ID: 5, Rate: 0.0, Name: '正一郎' },
+  { ID: 13, Rate: 0.1, Name: '清次郎' },
+  { ID: 25, Rate: 0.0, Name: '誠三郎' },
+  { ID: 42, Rate: 0.3, Name: '征史郎' }
+];
+
+const results = new Linq(parameters)
+  .Select(x => {
+    return { ID: x.ID, Name: x.Name };
+  })
+  .ToArray();
+
+const results2 = new Linq(parameters).Select(value => value.Name).ToArray();
+
+console.log('results:', results, results2);
---
+++ test/toDictionary.ts
@@ -0,0 +1,21 @@
+import Linq from '../src/linq.ts';
+
+let parameters = [
+  { ID: 0, Age: 52, Name: '正一郎' },
+  { ID: 8, Age: 28, Name: '清次郎' },
+  { ID: 3, Age: 20, Name: '誠三郎' },
+  { ID: 4, Age: 18, Name: '征史郎' }
+];
+
+let dictionary = new Linq(parameters).ToDictionary(x => x.ID).ToArray();
+let dictionary2 = new Linq(parameters)
+  .ToDictionary(x => {
+    return { ID: x.ID, Name: x.Name };
+  })
+  .ToArray();
+
+// dictionary3  = new jslinq(parameters).ToDictionary \
+//   ((value) -> value.ID, (value) -> value.Name).ToArray()
+console.log('dictionary:', dictionary);
+
+console.log('dictionary2:', dictionary2);
