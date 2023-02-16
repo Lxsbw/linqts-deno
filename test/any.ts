a879fba6c0bdef96543b473efef4cb40da6b74fe
zhixiong <lxsbw@outlook.com>
2023-02-16 10:22:25 +0800

    test branch

---
+++ test/any.ts
@@ -0,0 +1,23 @@
+let numbers, reaultA, reaultB, reaultC, reaultD;
+
+import Linq from '../src/linq.ts';
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+reaultA = new Linq<number>(numbers).Any(function (value) {
+  return value % 2 === 0;
+});
+
+reaultB = new Linq<number>(numbers).Any(function (value) {
+  return value >= 10;
+});
+
+reaultC = new Linq<number>(numbers).Any(x => x < 5);
+// reaultD = new Linq<number>(numbers).All(x => x < 5);
+
+console.log('reaultA:', reaultA);
+console.log('reaultB:', reaultB);
+console.log('reaultC:', reaultC);
+console.log('reaultD:', reaultD);
+
+// deno run ./test/any.ts
---
+++ test/firstOrDefault.ts
@@ -0,0 +1,23 @@
+let numbers, parameters, result;
+
+import Linq from '../src/linq.ts';
+interface Person {
+  ID: number;
+  Name: string;
+}
+
+numbers = [1, 2, 3, 5, 7, 11];
+result = new Linq(numbers).First();
+console.log('result:', result);
+
+result = new Linq<number>(numbers).First(x => x % 2 === 0);
+console.log('result:', result);
+
+parameters = [
+  { ID: 5, Name: '正一郎' },
+  { ID: 13, Name: '清次郎' },
+  { ID: 25, Name: '誠三郎' },
+  { ID: 42, Name: '征史郎' }
+];
+result = new Linq<Person>(parameters).FirstOrDefault(x => x.ID === 30);
+console.log('result:', result);
