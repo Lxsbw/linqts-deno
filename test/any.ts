f4418206e8eacc8ca082792a4d3c9196ed57cf9e
zhixiong <lxsbw@outlook.com>
2023-03-07 17:25:17 +0800

    unit test update

--- test/any.ts
+++ test/any.ts
@@ -4,16 +4,16 @@ import Linq from '../src/linq.ts';
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-reaultA = new Linq<number>(numbers).Any(function (value) {
+reaultA = new Linq<number>(numbers).any(function (value) {
   return value % 2 === 0;
 });
 
-reaultB = new Linq<number>(numbers).Any(function (value) {
+reaultB = new Linq<number>(numbers).any(function (value) {
   return value >= 10;
 });
 
-reaultC = new Linq<number>(numbers).Any(x => x < 5);
-// reaultD = new Linq<number>(numbers).All(x => x < 5);
+reaultC = new Linq<number>(numbers).any(x => x < 5);
+// reaultD = new Linq<number>(numbers).all(x => x < 5);
 
 console.log('reaultA:', reaultA);
 console.log('reaultB:', reaultB);
